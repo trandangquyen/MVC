3a4162aa00e5a7793c6d47f76471ab6467a35872
quangtruong <quangtr.work@gmil.com>
2017-04-26 18:43:36 +0700

    Khoi tao trang list cac tin tuc

---
+++ application/views/tintuc/list.php
@@ -0,0 +1,260 @@
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/1">read more...</a>
+
+</p>
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/2">read more...</a>
+
+</p>
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/3">read more...</a>
+
+</p>
+
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/wordpress">read more...</a>
+
+</p>
+
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/wordpress">read more...</a>
+
+</p>
+
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/wordpress">read more...</a>
+
+</p>
+
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/wordpress">read more...</a>
+
+</p>
+
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/wordpress">read more...</a>
+
+</p>
+
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/wordpress">read more...</a>
+
+</p>
+
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/wordpress">read more...</a>
+
+</p>
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/wordpress">read more...</a>
+
+</p>
+
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/wordpress">read more...</a>
+
+</p>
+
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/wordpress">read more...</a>
+
+</p>
+
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/wordpress">read more...</a>
+
+</p>
+
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/wordpress">read more...</a>
+
+</p>
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/wordpress">read more...</a>
+
+</p>
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/1">read more...</a>
+
+</p>
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/2">read more...</a>
+
+</p>
+<h3 class='head3'>WordPress</h3>
+
+<p class='content'>
+WordPress 4.7.4 Maintenance Release
+Posted April 20, 2017 by Pascal Birchler. Filed under Releases.
+
+After almost sixty million downloads of WordPress 4.7, we are pleased to announce the immediate availability of WordPress 4.7.4, a maintenance release.
+
+This release contains 47 maintenance fixes and enhancements, chief among them an incompatibility between the upcoming Chrome version and the visual editor, inconsistencies in media handling, and further improvements to the REST API. For a full list of changes, consult the release notes and the list of changes.
+<br />
+<a class="readmore" href="index.php/tintuc/details/3">read more...</a>
+
+</p>
+
+
