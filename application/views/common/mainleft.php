676df2fee966aaca430e29c6bc2c3e5a7b5ed0b3
quangtruong <quangtr.work@gmil.com>
2017-04-27 18:27:57 +0700

    thêm mainleft.php, mainright.php

---
+++ application/views/common/mainleft.php
@@ -0,0 +1,37 @@
+<div class="col-xs-3">
+  <div id="main_left">
+
+    <div class="tittle-box-left">
+      <img src="public/themes/images/icon.png" alt="" class="icon_tittle">
+      <h2>Danh mục sản phẩm</h2>
+    </div> <!-- end tittle -->
+    <div class="boder_menu">
+      <ul>
+        <span>Tivi</span>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">Toshiba</a></li>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">Samsung</a></li>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">Panasonic</a></li>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">LG</a></li>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">JVC</a></li>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">AKAI</a></li>
+      </ul>
+      <ul>
+        <span>Điện thoại</span>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">HTC</a></li>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">BlackBerry</a></li>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">Asus</a></li>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">Apple</a></li>
+      </ul>
+      <ul>
+        <span>Laptop</span>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">HP</a></li>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">Dell</a></li>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">Asus</a></li>
+        <li><img src="public/themes/images/icon-02.png" alt=""><a href="#">Apple</a></li>
+        <li class="end_menu"><img src="public/themes/images/icon-02.png" alt=""><a href="#">Acer</a></li>
+      </ul>
+    </div> <!-- end boder_menu -->
+
+  </div><!-- end main_left -->
+</div> <!-- end col-xs-3 -->
+<div class="col-xs-6">
