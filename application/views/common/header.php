a1bdb2b807b1d7744832f05067dbf555be83fb31
trandangquyen <trandangquyen@gmail.com>
2017-04-28 17:28:09 +0700

    edit file lienhe.css

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
 
-        <base href="http://localhost/mvc_grop/MVC/">
+        <base href="http://localhost/training/MVC/">
         <meta charset="utf-8">
         <title>Training E-commerce Templates</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
