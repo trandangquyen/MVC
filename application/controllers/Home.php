3460affc2f2154d5735c9a32de012363810ec8b1
trandangquyen <trandangquyen@gmail.com>
2017-05-04 08:54:28 +0700

    edit home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -26,7 +26,6 @@ class Home extends CI_Controller {
 	public function index($page = 'site/home')
 	{
 		$this->load->model('Products_model');
-		echo APPPATH.'views/'.$page.'.php';
 		if ( ! file_exists(APPPATH.'views/'.$page.'.php'))
         {
                 // Whoops, we don't have a page for that!
