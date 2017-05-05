80945e656cea3d56e20813222606207cd3b9c76c
quangtruong <quangtr.work@gmil.com>
2017-05-05 13:34:59 +0700

    sửa mainleft.php mainright.php

---
+++ application/views/site/common/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <base href="<?php echo base_url(); ?>">
+        <meta charset="utf-8">
+        <title><?=isset($title) ? $title : 'Training E-commerce Templates'?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
+        <!-- bootstrap -->
+        <link href="public/bootstrap/css/bootstrap.min.css" rel="stylesheet">      
+        <link href="public/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <!-- Css for @media screen -->
+        <link href="public/themes/css/bootstrappage.css" rel="stylesheet"/>		
+        <!-- global styles -->
+        <link href="public/themes/css/flexslider.css" rel="stylesheet"/>
+        <link href="public/themes/css/lienhe.css" rel="stylesheet"/>
+        <link href="public/themes/css/main.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div id="wrapper" class="container">
+            <!-- Begin Header -->
+            <div class="header">
+				<div id="Icon_search">
+                    <img src="public/images/header.png" alt="">
+                </div>
+
+                <ul id='top-menu'>
+                    <li class='item<?=(isset($active) && $active == 'trangchu') ? ' current' : '' ?>' id="lintrangchu"><a href='home'>Trang chủ</a></li>
+                    <li class='item_separator'><img  class = "anhmenu" src="public/images/anhmenu.png" alt="đường viền cách giữa menu" /> </li>
+                    <li class='item<?=(isset($active) && $active == 'sanpham') ? ' current' : '' ?>' id="lintrangchu"><a href='sanpham'>Sản phẩm</a></li><li class='item_separator'><img  class = "anhmenu" src="public/images/anhmenu.png" alt="đường viền cách giữa menu" /> </li>
+                    <li class='item<?=(isset($active) && $active == 'gioithieu') ? ' current' : '' ?>' id="lintrangchu"><a href='gioithieu/view'>Giới thiệu</a></li>
+                    <li class='item_separator'><img  class = "anhmenu" src="public/images/anhmenu.png" alt="đường viền cách giữa menu" /> </li>
+                    <li class='item<?=(isset($active) && $active == 'tintuc') ? ' current' : '' ?>' id="lintrangchu"><a href='tintuc/list'>Tin tức</a>
+                    </li>
+                    <li class='item_separator'><img  class = "anhmenu" src="public/images/anhmenu.png" alt="đường viền cách giữa menu" /> </li>
+                    <li class='item<?=(isset($active) && $active == 'lienhe') ? ' current' : '' ?>' id="lintrangchu"><a href='lienhe/view'>Liên hệ</a></li>
+                    <li class='item_separator'><img  class = "anhmenu" src="public/images/anhmenu.png" alt="đường viền cách giữa menu" /> </li>
+
+                </ul>			
+            </div>
+            <!-- End Header -->
+            <!-- Begin main content -->
+            <div id="main-content">
+            <!-- End Header -->
+		<!-- Begin main content -->
+		<div id="main-content">
+			<div class="container">
+		      	<div class="row">
---
+++ application/views/site/common/mainleft.php
@@ -0,0 +1,24 @@
+<div class="col-xs-3">
+  <div id="main_left">
+
+    <div class="tittle-box">
+      <img src="public/themes/images/icon.png" alt="" class="icon_tittle">
+      <h3><?=$title?></h3>
+    </div> <!-- end tittle -->
+    <div class="boder_menu">
+    <?php 
+    foreach ($category as $mainCategory) {
+      //var_dump($mainCategory);
+      echo '<ul>
+        <span><a href="index.php/theloai/'.$mainCategory['id'].'">'.$mainCategory['name'].'</a></span>';
+        if(!empty($mainCategory['data'])) foreach ($mainCategory['data'] as $subCategory) {
+          echo '<li><img src="public/themes/images/icon-02.png" alt="" class="icon_list"><a href="index.php/theloai/'.$subCategory['id'].'">'.$subCategory['name'].'</a></li>';
+        }
+      echo '</ul>';
+      }
+       ?>
+
+    </div> <!-- end boder_menu -->
+
+  </div><!-- end main_left -->
+</div> <!-- end col-xs-3 -->
