a21858ead0886ef2540de249d46b7db581363496
mrhieuit91 <mrhieuit91@gmail.com>
2017-05-12 09:09:23 +0700

    fix vị trí google map

--- application/views/site/lienhe.php
+++ application/views/site/lienhe.php
@@ -81,8 +81,8 @@
 	                <h3>Thông tin liên hệ</h3>
 	                <div>
 	                    P618, Chung cư Tân Phúc<br>
-	                    Đống Đa<br>
-	                    Hà nội
+	                    Vinh<br>
+	                    Nghệ An
 	                </div>
 	               <br>
 	               <table>
@@ -117,9 +117,9 @@
 
 <script>
 function myMap() {
-  var myCenter = new google.maps.LatLng(51.508742,-0.120850);
+  var myCenter = new google.maps.LatLng(18.662115,105.682223);
   var mapCanvas = document.getElementById("map");
-  var mapOptions = {center: myCenter, zoom: 5};
+  var mapOptions = {center: myCenter, zoom: 17};
   var map = new google.maps.Map(mapCanvas, mapOptions);
   var marker = new google.maps.Marker({position:myCenter});
   marker.setMap(map);
