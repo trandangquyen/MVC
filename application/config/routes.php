51e8b7dceebb57b33178298f1f400b2e0b14f229
trandangquyen <trandangquyen@gmail.com>
2017-05-13 09:49:18 +0700

    add routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -28,10 +28,16 @@ $route['admin/category'] = 'admin/category';
 $route['admin/category/add'] = 'admin/category/addCategory';
 $route['admin/category/delete/(:num)'] = 'admin/category/deteleCategory/$1';
 $route['admin/category/edit/(:num)'] = 'admin/category/updateCategory/$1';
-
 $route['admin/news'] = 'admin/news';
 $route['admin/news/add'] = 'admin/news/addNews';
 $route['admin/news/delete/(:num)'] = 'admin/news/deteleNews/$1';
 $route['admin/news/edit/(:num)'] = 'admin/news/updateNews/$1';
-
+$route['admin-sanpham'] = 'adminsanpham';
+$route['admin-tintuc'] = 'admintintuc';
+$route['admin-gioithieu'] = 'admingioithieu';
+$route['admin-lienhe'] = 'adminlienhe';
+$route['admin-caidat'] = 'admincaidat';
+$route['admin-user'] = 'adminuser';
+$route['admin-category'] = 'admincategory';
+$route['admin-home'] = 'adminhome';
 
