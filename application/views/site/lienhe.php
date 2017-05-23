5d63651ed4ccbde53e40659c7084fd7776f39899
KhoaZero123 <khoazero123@gmail.com>
2017-05-23 11:37:29 +0700

    fix some

--- application/views/site/lienhe.php
+++ application/views/site/lienhe.php
@@ -115,9 +115,9 @@
 
 <script>
 function myMap() {
-  var myCenter = new google.maps.LatLng(51.508742,-0.120850);
+  var myCenter = new google.maps.LatLng(18.662085,105.6800203);
   var mapCanvas = document.getElementById("map");
-  var mapOptions = {center: myCenter, zoom: 5};
+  var mapOptions = {center: myCenter, zoom: 15};
   var map = new google.maps.Map(mapCanvas, mapOptions);
   var marker = new google.maps.Marker({position:myCenter});
   marker.setMap(map);
